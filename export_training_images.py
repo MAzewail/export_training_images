39023f85e14d055442963f31402c59af6a6e0644
unknown <mohammed.abdallah.salem@gmail.com>
2023-01-07 16:27:55 +0200

    V0.1

--- export_training_images.py
+++ export_training_images.py
@@ -3,9 +3,9 @@ import cv2
 
 v = cv2.VideoCapture("/content/my_video.mp4")
 
-
+numer_of_frames = 100
 frameN = 0
-while frameN <=3:
+while frameN <= number_of_frames:
     s, frame = v.read()
     if s :
         path = '/content/drive/MyDrive/My_project/train/' + str(frameN) + '.jpg'
